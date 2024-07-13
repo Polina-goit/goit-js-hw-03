eabb90dd63644ffa8300b3785645917d49d9dc94
Polina <polinakoctyu4enko@gmail.com>
2024-07-13 18:31:09 +0300

    ch3

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function makeArray(firstArray, secondArray, maxLength) {
     let newArray = firstArray.concat(secondArray);
     
     if (newArray.length > maxLength) {
-        return newArray.slice(maxLength);
+        return newArray.slice(0, maxLength);
     } else {
         return newArray;
     }
